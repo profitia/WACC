c89661c4b35cb3596ce58d03fde65519f9e8460d
Tomasz Uscinski <tomaszuscinski@MacBook-Air-M1.local>
2026-04-25 11:04:42 +0200

    fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,12 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
